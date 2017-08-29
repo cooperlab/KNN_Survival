14df6c006bc5e89e332fc1fec213cde75563cb82
Mohamed Amgad <mtageld@emory.edu>
2017-08-28 22:14:33 -0400

    nca using only np and scp

--- SurvivalNCA.py
+++ SurvivalNCA.py
@@ -21,12 +21,11 @@ cwd = os.getcwd()
 conditionalAppend(cwd)
 
 import numpy as np
-import SurvivalUtils as sUtils
-
 from scipy.io import loadmat, savemat
 import scipy.optimize as opt
 
-#tf.logging.set_verbosity(tf.logging.INFO)
+import SurvivalUtils as sUtils
+import nca_cost
 
 
 #raise Exception()
@@ -47,9 +46,7 @@ Survival = np.int32(Data['Survival'])
 Censored = np.int32(Data['Censored'])
 fnames = Data['Integ_Symbs']
 
-# Get split indices
-#splitIdxs = sUtils.getSplitIdxs(data)
-
+# Limit patient no for prototyping
 n = 50
 data = data[0:n,:]
 Survival = Survival[0:n,:]
@@ -57,10 +54,9 @@ Censored = Censored[0:n,:]
 
 # remove zero-variance features
 fvars = np.std(data, 0)
-toKeep = fvars > 0
-data = data[:, toKeep]
-fnames = fnames[toKeep]
-fvars = fvars[toKeep]
+keep = fvars > 0
+data = data[:, keep]
+fnames = fnames[keep]
 
 # Generate survival status - discretized into months
 aliveStatus = sUtils.getAliveStatus(Survival, Censored, scale = 30)
@@ -72,9 +68,13 @@ aliveStatus = sUtils.getAliveStatus(Survival, Censored, scale = 30)
 
 RESULTPATH = "/home/mohamed/Desktop/CooperLab_Research/KNN_Survival/Results/tmp/"
 
-LEARN_RATE = 0.05
-D_new = data.shape[1] # set D_new < D to reduce dimensions
+DIMS = 100 #data.shape[1] # set DIMS < D to reduce dimensions
+
+OBJECTIVE = 'Mahalanobis'
+THRESH = None # None or between [0, 1], the smaller the more emphasis on closer neighbors
+#MAXITER = 100
 MONITOR_STEP = 10
+
 
 
 #%%============================================================================
@@ -82,52 +82,37 @@ MONITOR_STEP = 10
 #==============================================================================
 
 # Get dims
-N, D = np.int32(data.shape)
-T = np.int32(aliveStatus.shape[1]) # no of time points
+N, D = data.shape
+T = aliveStatus.shape[1] # no of time points
 
 # Initialize A to a scaling matrix
-A_init = np.zeros((D, D_new))
-
-epsilon = 1e-7 # to  avoid division by zero
-
-np.fill_diagonal(A_init, 1./(data.max(axis=0) - data.min(axis=0) + epsilon))
-#np.fill_diagonal(A_init, 1./(np.sqrt(fvars) + epsilon))
-#np.fill_diagonal(A_init, np.random.rand(D))
-#A_init = np.random.rand(D, D_new)
-
-A_init = np.float32(A_init)
+A = np.eye(D, DIMS)
 
 
 #%%============================================================================
-# 
+# Go through time points
 #==============================================================================
 
+# initialize cum_f and cum_gradf
+cum_f = 0
+cum_gradf = np.zeros(A.shape)
 
-#%%============================================================================
-# Now parse the learned matrix A and save
-#==============================================================================
+#t = 20
+for t in range(T):
 
-#def getRanks(A):
-#    w = np.diag(A).reshape(D_new, 1)
-#    fidx = np.arange(len(A)).reshape(D_new, 1)
-#    
-#    w = np.concatenate((fidx, w), 1)
-#    w = w[w[:,1].argsort()][::-1]
-#    tokeep = w[:,1] < 100000
-#    w = w[tokeep,:]
-#    
-#    fnames_ranked = fnames[np.int32(w[:,0])]
-#    
-#    return fnames_ranked
-#
-#ranks_init = getRanks(A_init)
-#ranks_current = getRanks(A_current)
-#
-## Save analysis result
-#result = {'A_init': A_init,
-#          'A_current': A_current,
-#          'ranks_init': ranks_init,
-#          'ranks_current': ranks_current,
-#          'LEARN_RATE': LEARN_RATE,}
-#
-#savemat(RESULTPATH + 'result', result)
+    print("t = {} of {}".format(t, T-1))
+        
+    # Get patients with known survival status at time t
+    Y = aliveStatus[:, t]
+    keep = Y >= 0
+    Y = Y[keep]
+    X = data[keep, :]
+    
+    if OBJECTIVE == 'Mahalanobis':
+        f, gradf = nca_cost.cost(A.T, X.T, Y, threshold=THRESH)
+    elif OBJECTIVE == 'KL-divergence':
+        f, gradf = nca_cost.cost_g(A.T, X.T, Y, threshold=THRESH)
+        
+    cum_f += f
+    cum_gradf += gradf.T
+

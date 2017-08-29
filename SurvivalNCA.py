adb360c6e8deec68f566484cbb5b80dd4d848938
Mohamed Amgad <mtageld@emory.edu>
2017-08-29 16:37:53 -0400

    WIP - convert NCA to OOP

--- SurvivalNCA.py
+++ SurvivalNCA.py
@@ -66,76 +66,72 @@ aliveStatus = sUtils.getAliveStatus(Survival, Censored, scale = 30)
 # --- P R O T O T Y P E S -----------------------------------------------------
 #==============================================================================
 
-RESULTPATH = "/home/mohamed/Desktop/CooperLab_Research/KNN_Survival/Results/tmp/"
+self.RESULTPATH = "/home/mohamed/Desktop/CooperLab_Research/KNN_Survival/Results/tmp/"
+self.description = "nca_GBMLGG_"
 
-DIMS = data.shape[1] # set DIMS < D to reduce dimensions
+self.DIMS = data.shape[1] # set DIMS < D to reduce dimensions
 
-OBJECTIVE = 'Mahalanobis'
-THRESH = None # None or between [0, 1], the smaller the more emphasis on closer neighbors
-LEARN_RATE = 0.01
-MONITOR_STEP = 1
+self.OBJECTIVE = 'Mahalanobis'
+self.THRESH = None # None or between [0, 1], the smaller the more emphasis on closer neighbors
+self.LEARN_RATE = 0.01
+self.MONITOR_STEP = 1
 
 # no of patients chosen randomly each time point
-N_SUBSET = 10
+self.N_SUBSET = 10
 
 #%%============================================================================
 # Setting things up
 #==============================================================================
 
 # Get dims
-N, D = data.shape
-T = aliveStatus.shape[1] # no of time points
+self.N, self.D = self.data.shape
+self.T = aliveStatus.shape[1] # no of time points
 epsilon = 1e-7 # to  avoid division by zero
 
 # Initialize A to a scaling matrix
 #A = np.eye(D, DIMS)
-A = np.zeros((D, DIMS))
-np.fill_diagonal(A, 1./(data.max(axis=0) - data.min(axis=0) + epsilon))
+self.A = np.zeros((D, DIMS))
+np.fill_diagonal(self.A, 
+                 1./(self.data.max(axis=0) - self.data.min(axis=0) + epsilon))
 
-#A = (1./(data.max(axis=0) - data.min(axis=0) + epsilon)).reshape(D, 1)
-#A = np.ones([D, 1])
-
-#A_init = A.copy()
 
 
 #%%============================================================================
-# Define objective function
+# Core model
 #==============================================================================
 
-def survival_nca_cost(A, data, aliveStatus, 
-                      OBJECTIVE= 'Mahalanobis', THRESH = None,
-                      N_SUBSET = 20):
+def _survival_nca_cost(self):
     
     """Gets cumulative cost and gradient for all time points"""
 
     # initialize cum_f and cum_gradf
     cum_f = 0
-    cum_gradf = np.zeros(A.shape)
+    cum_gradf = np.zeros(self.A.shape)
     
     #t = 20
-    for t in range(T):
+    for t in range(self.T):
     
-        print("t = {} of {}".format(t, T-1))
+        print("t = {} of {}".format(t, self.T-1))
             
         # Get patients with known survival status at time t
-        Y = aliveStatus[:, t]
+        Y = self.aliveStatus[:, t]
         keep = Y >= 0 
         # proceed only if there's enough known patients
-        if np.sum(0 + keep) < N_SUBSET:
+        if np.sum(0 + keep) < self.N_SUBSET:
             print("skipping current t ...")
             continue 
         Y = Y[keep]
-        X = data[keep, :]
+        X = self.data[keep, :]
         
         # keep a random subset of patients (for efficiency)
-        keep = np.random.randint(0, X.shape[0], N_SUBSET)
+        keep = np.random.randint(0, X.shape[0], self.N_SUBSET)
         Y = Y[keep]
         X = X[keep, :]
         
-        if OBJECTIVE == 'Mahalanobis':
-            f, gradf = nca_cost.cost(A.T, X.T, Y, threshold=THRESH)
-        elif OBJECTIVE == 'KL-divergence':
-            f, gradf = nca_cost.cost_g(A.T, X.T, Y, threshold=THRESH)
+        if self.OBJECTIVE == 'Mahalanobis':
+            f, gradf = nca_cost.cost(self.A.T, X.T, Y, threshold=self.THRESH)
+        elif self.OBJECTIVE == 'KL-divergence':
+            f, gradf = nca_cost.cost_g(self.A.T, X.T, Y, threshold=self.THRESH)
             
         cum_f += f
         cum_gradf += gradf.T # sum of derivative is derivative of sum
@@ -143,77 +139,134 @@ def survival_nca_cost(A, data, aliveStatus,
     return [cum_f, cum_gradf]
 
 
-#%%============================================================================
-# Optimize objective function
+#==========================================================================
+
+def train(self):
+    
+    """ learns feature matrix A to minimize objective function"""
+    
+    self.costs = []
+    step = 0
+     
+    try: 
+        while True:
+            
+            print("\n--------------------------------------------")
+            print("---- STEP = " + str(step))
+            print("--------------------------------------------\n")
+            
+            [cum_f, cum_gradf] = self._survival_nca_cost(self.A)
+            # update A
+            self.A -= self.LEARN_RATE * cum_gradf
+            
+            # Discard non-diagnoal terms - 
+            # Uncomment if you just want linear scaling of features,
+            # but expect worse performance without much gain in 
+            # interpretability. (columns of Ax still correspond to 
+            # columns of x without this step)
+            # A *= np.eye(A.shape[0], A.shape[1])
+    
+            # update costs
+            self.costs.append([step, cum_f])
+            
+            # monitor
+            if (step % self.MONITOR_STEP == 0) and (step > 0):
+                cs = np.array(self.costs)
+                self._plotMonitor(arr= cs, title= "cost vs. epoch", 
+                                  xlab= "epoch", ylab= "cost", 
+                                  savename= self.RESULTPATH + 
+                                  self.description + "cost.svg")
+            
+            step += 1
+            
+    except KeyboardInterrupt:
+        print("\n Finished training model.")
+
+
+#==========================================================================
+
+def rankFeats(self):
+    
+    """rank features by how variant they are after the new transformation"""
+
+    def _getRanks(A):
+        
+        Ax = np.dot(self.data, A)
+    
+        fvars = np.std(Ax, 0).reshape(self.DIMS, 1)
+        fidx = np.arange(len(A)).reshape(self.DIMS, 1)
+        
+        fvars = np.concatenate((fidx, fvars), 1)
+        fvars = fvars[fvars[:,1].argsort()][::-1]
+        
+        fnames_ranked = fnames[np.int32(fvars[:,0])]
+        
+        return fvars, fnames_ranked
+    
+    self.fvars_init, self.ranks_init = _getRanks(np.eye(self.D, self.DIMS))
+    self.fvars, self.ranks = _getRanks(self.A)
+
+
+#%%===========================================================================
+# Helper methods
 #==============================================================================
 
-costs = []
-step = 0
- 
-try: 
-    while True:
-        
-        print("\n--------------------------------------------")
-        print("---- STEP = " + str(step))
-        print("--------------------------------------------\n")
-        
-        [cum_f, cum_gradf] = survival_nca_cost(A, data, aliveStatus, 
-                                               OBJECTIVE = OBJECTIVE, 
-                                               THRESH = THRESH, 
-                                               N_SUBSET = N_SUBSET)
-        # update A
-        A -= LEARN_RATE * cum_gradf
-        
-        # Discard non-diagnoal terms - 
-        # Uncomment if you just want linear scaling of features,
-        # but expect worse performance without much gain in 
-        # interpretability. (columns of Ax still correspond to 
-        # columns of x without this step)
-        # A *= np.eye(A.shape[0], A.shape[1])
-
-        # update costs
-        costs.append([step, cum_f])
-        
-        # monitor
-        if step % MONITOR_STEP == 0:
-            cs = np.array(costs)
-            sUtils.plotMonitor(arr= cs, title= "cost vs. epoch", 
-                               xlab= "epoch", ylab= "cost", 
-                               savename= RESULTPATH + "cost.svg")
-        
-        step += 1
-        
-except KeyboardInterrupt:
-    pass
-
-#%%============================================================================
-# Now rank features by how variant they are after the new transformation
-#==============================================================================
-
-def getRanks(A):
+# The following load/save methods are inspired by:
+    # https://stackoverflow.com/questions/2345151/
+    # how-to-save-read-class-wholly-in-python
     
-    Ax = np.dot(data, A)
+def save(self):
+    
+    """save class as ModelAttributes.txt"""
+    
+    print("Saving model attributes ...")
+    self._updateStepCount()
+    with open(self.RESULTPATH + self.description + 'ModelAttributes.txt','wb') as file:
+        file.write(_pickle.dumps(self.__dict__))
+        file.close()
 
-    fvars = np.std(Ax, 0).reshape(DIMS, 1)
-    fidx = np.arange(len(A)).reshape(DIMS, 1)
-    
-    fvars = np.concatenate((fidx, fvars), 1)
-    fvars = fvars[fvars[:,1].argsort()][::-1]
-    
-    fnames_ranked = fnames[np.int32(fvars[:,0])]
-    
-    return fvars, fnames_ranked
+#==========================================================================
 
-fvars_init, ranks_init = getRanks(np.eye(D, DIMS))
-fvars, ranks = getRanks(A)
+def load(self, LOADPATH):
+    
+    """try to load ModelAttributes.txt"""
+    
+    print("Loading model attributes ...")
+    with open(LOADPATH,'rb') as file:
+        dataPickle = file.read()
+        file.close()
+        self.__dict__ = _pickle.loads(dataPickle)
+        
+#==========================================================================
+
+def _plotMonitor(self, arr, title, xlab, ylab, savename):
+                    
+    """ plots cost/other metric to monitor progress """
+    
+    print("Plotting " + title)
+    
+    fig, ax = plt.subplots() 
+    ax.plot(arr[:,0], arr[:,1], 'b', linewidth=1.5, aa=False)
+    plt.title(title, fontsize =16, fontweight ='bold')
+    plt.xlabel(xlab)
+    plt.ylabel(ylab) 
+    plt.tight_layout()
+    plt.savefig(savename)
+    plt.close() 
+
+
+#%%
+#%%
+#%%
+#%%
+result = 
 
 # Save analysis result
-result = {'A': A,
-          'ranks_init': ranks_init,
-          'ranks': ranks,
-          'LEARN_RATE': LEARN_RATE,}
+result = {'A': self.A,
+          'ranks_init': self.ranks_init,
+          'ranks': self.ranks}
 
-savemat(RESULTPATH + 'result', result)
+savemat(self.RESULTPATH + self.description + 'result', result)
 
 
 #%%============================================================================

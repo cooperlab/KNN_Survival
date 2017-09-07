20fc5ce1bb36ca8acabce682c5c71e8f3ed38bbe
Mohamed Amgad <mtageld@emory.edu>
2017-09-07 10:37:24 -0400

    still trying to get loop pij to work

--- computational_graph.py
+++ computational_graph.py
@@ -127,18 +127,18 @@ class comput_graph(object):
             
             with tf.name_scope("pij_loop"):
                 
-                #n = tf.shape(self.X_transformed)[0]
-                #n = tf.cast(n, tf.int32)
+                n = tf.shape(self.X_transformed)[0]
+                n = tf.cast(n, tf.int32)
                 #n = self.X_transformed.get_shape().as_list()[0]
-                n = tf.cast(tf.size(self.T)-1, tf.int32)
+                #n = tf.cast(tf.size(self.T)-1, tf.int32)
                 
                 # first patient
-                sID = 0
-                patient = self.X_transformed[sID, :]
-                patient_normax = (patient[None, :] - self.X_transformed)**2
-                normAX = tf.reduce_sum(patient_normax, axis=1)
-                normAX = normAX[None, :]
-                #normAX = tf.Variable(tf.zeros([n, n]))
+                #sID = 0
+                #patient = self.X_transformed[sID, :]
+                #patient_normax = (patient[None, :] - self.X_transformed)**2
+                #normAX = tf.reduce_sum(patient_normax, axis=1)
+                #normAX = normAX[None, :]
+                normAX = tf.Variable(tf.zeros([n, n]), validate_shape=False)
                 
                 # all other patients
                 def _append_normAX(sID, normAX):
@@ -149,10 +149,13 @@ class comput_graph(object):
                     patient = self.X_transformed[sID, :]
                     patient_normax = (patient[None, :] - self.X_transformed)**2
                     patient_normax = tf.reduce_sum(patient_normax, axis=1)
+                    patient_normax = patient_normax[None, :]
                 
                     # append to existing list
-                    normAX = tf.concat((normAX, patient_normax[None, :]), axis=0)
+                    #normAX = tf.concat((normAX, patient_normax[None, :]), axis=0)
+                    #a = normAX[sID, :]
                     #normAX = tf.assign(normAX[sID, :], patient_normax[None, :])
+                    normAX = normAX[sID, :].assign(patient_normax[None, :])
                     
                     # sID++
                     sID = tf.cast(tf.add(sID, 1), tf.int32)
@@ -160,17 +163,19 @@ class comput_graph(object):
                     return sID, normAX
                     
                 
+                
+                
                 # Go through all patients and add normAX
-                sID = tf.cast(tf.Variable(1), tf.int32)
-                #sID = tf.cast(tf.Variable(0), tf.int32)
+                #sID = tf.cast(tf.Variable(1), tf.int32)
+                sID = tf.cast(tf.Variable(0), tf.int32)
                 
                 c = lambda sID, normAX: tf.less(sID, tf.cast(n, tf.int32))
                 b = lambda sID, normAX: _append_normAX(sID, normAX)
                 
                 (sID, normAX) = tf.while_loop(c, b, 
-                                loop_vars = [sID, normAX], 
-                                shape_invariants = 
-                                [sID.get_shape(), tf.TensorShape([None, n])])
+                                loop_vars = [sID, normAX])#, 
+                                #shape_invariants = 
+                                #[sID.get_shape(), tf.TensorShape([None, n])])
                             
             return normAX
         

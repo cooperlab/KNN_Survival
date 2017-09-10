c833e60b7a7cfdb74bbb6ae950fc064eaf8fa134
Mohamed Amgad <mtageld@emory.edu>
2017-09-10 19:07:09 -0400

    WIP - KNN survival class

--- junk/KNNSurvival.py
+++ junk/KNNSurvival.py
@@ -8,28 +8,126 @@ Created on Sat Aug 26 00:16:36 2017
 Predict survival using KNN
 """
 
-#def KNN_Survival(X_test,X_train,Alive_train,K,Beta1,Filters,sigma,Lambda)
+# Append relevant paths
+import os
+import sys
 
-"""
- This predicts survival based on the labels of K-nearest neighbours 
- using weighted euclidian distance and non-cumulative survival probability.
+def conditionalAppend(Dir):
+    """ Append dir to sys path"""
+    if Dir not in sys.path:
+        sys.path.append(Dir)
 
- INPUTS: 
- --------
- IMPORTANT: features as rows, samples as columns
- WARNING: Input data has to be normalized to have similar scales
- 
- X_test - testing sample features
- X_train - training sample features
- Alive_train - Alive dead status (+1 (alive) --> -1 (dead))
- K - number of nearest-neighbours to use
- Beta - shrinkage factor --> higher values indicate less important
-        features
- Filters - method of emphasizing or demphasizing neighbours 
- sigma - sigma of gaussian filter (lower values result in more emphasis on 
-                                   closer neighbours)
- Lambda -  the larger lambda, the less penalty on lack of common
-           dimensions (when there are no NAN values and lamda = 1, 
-           there's no dimension penlty)
-"""
+cwd = os.getcwd()
+conditionalAppend(cwd)
 
+#import _pickle
+import numpy as np
+#from matplotlib import cm
+#import matplotlib.pylab as plt
+
+import logging
+import datetime
+
+import ProjectUtils as pUtils
+import SurvivalUtils as sUtils
+#import DataManagement as dm
+
+#raise(Exception)
+
+#%%============================================================================
+# NCAmodel class (trainable model)
+#==============================================================================
+
+class SurvivalKNN(object):
+    
+    """
+    KNN Survival.
+    
+    This predicts survival based on the labels of K-nearest neighbours 
+    using weighted euclidian distance and non-cumulative survival probability.
+        
+    """
+    
+    # Init
+    ###########################################################################
+    
+    def __init__(self, 
+                 RESULTPATH, description=""):
+        
+        """Instantiate a survival NCA object"""
+        
+        # Set instance attributes
+        #==================================================================
+        
+        self.RESULTPATH = RESULTPATH
+        self.LOGPATH = self.RESULTPATH + "logs/"
+        
+        # prefix to all saved results
+        self.description = description
+        
+        
+        # Create output dirs
+        #==================================================================
+        
+        self._makeSubdirs()
+        
+        # Configure logger - will not work with iPython
+        #==================================================================
+        
+        timestamp = str(datetime.datetime.today()).replace(' ','_')
+        logging.basicConfig(filename = self.LOGPATH + timestamp + "_RunLogs.log", 
+                            level = logging.INFO,
+                            format = '%(levelname)s:%(message)s')
+                            
+    #%%===========================================================================
+    # Miscellaneous methods
+    #==============================================================================
+    
+    def getModelInfo(self):
+        
+        """ Returns relevant model attributes"""
+        
+        attribs = {
+            'RESULTPATH' : self.RESULTPATH,
+            'description' : self.description,
+            'LOGPATH': self.LOGPATH,
+            }
+        
+        return attribs
+
+    #==========================================================================    
+    
+    def _makeSubdirs(self):
+        
+        """ Create output directories"""
+        
+        # Create relevant result subdirectories
+        pUtils.makeSubdir(self.RESULTPATH, 'plots')
+        pUtils.makeSubdir(self.RESULTPATH, 'preds')
+        pUtils.makeSubdir(self.RESULTPATH, 'logs')
+        
+
+    #%%===========================================================================
+    # Actual prediction model
+    #==============================================================================        
+        
+    def predict(self, X_test, 
+                X_train, 
+                Survival_train, Censored_train, K):
+        
+        """
+        Predict testing set using 'prototype' (i.e. training) set using KNN
+        
+        Args: 
+        --------
+        WARNING:   Unless preceeded by NCA, input data has to be normalized
+        to have similar scales
+         
+        X_test      - testing sample features; (N, D) np array
+        X_train     - training sample features; (N, D) np array
+        Survival_train - training sample time-to-event; (N,) np array
+        Censored_train - training sample censorship status; (N,) np array
+        K           - number of nearest-neighbours to use, int
+        """
+        
+        
---
+++ unit_tests/test_KNNSurvival.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep  9 16:54:33 2017
+
+@author: mohamed
+"""
+
+import sys
+sys.path.append('/home/mohamed/Desktop/CooperLab_Research/KNN_Survival/Codes')
+
+from scipy.io import loadmat
+import numpy as np
+
+import DataManagement as dm
+import NCA_model as nca
+
+#%%========================================================================
+# Prepare inputs
+#==========================================================================
+
+print("Loading and preprocessing data.")
+
+# Load data
+
+projectPath = "/home/mohamed/Desktop/CooperLab_Research/KNN_Survival/"
+#projectPath = "/home/mtageld/Desktop/KNN_Survival/"
+
+dpath = projectPath + "Data/SingleCancerDatasets/GBMLGG/Brain_Integ.mat"
+#dpath = projectPath + "Data/SingleCancerDatasets/GBMLGG/Brain_Gene.mat"
+#dpath = projectPath + "Data/SingleCancerDatasets/BRCA/BRCA_Integ.mat"
+
+Data = loadmat(dpath)
+
+Features = np.float32(Data['Integ_X'])
+#Features = np.float32(Data['Gene_X'])
+
+N, D = Features.shape
+
+if np.min(Data['Survival']) < 0:
+    Data['Survival'] = Data['Survival'] - np.min(Data['Survival']) + 1
+
+Survival = np.int32(Data['Survival']).reshape([N,])
+Censored = np.int32(Data['Censored']).reshape([N,])
+fnames = Data['Integ_Symbs']
+#fnames = Data['Gene_Symbs']
+
+RESULTPATH = projectPath + "Results/tmp/"
+MONITOR_STEP = 10
+description = "GBMLGG_Gene_"
+
+# remove zero-variance features
+fvars = np.std(Features, 0)
+keep = fvars > 0
+Features = Features[:, keep]
+fnames = fnames[keep]
+
+# Get split indices
+splitIdxs = dm.get_balanced_SplitIdxs(Censored)
+
+
+
+#%%============================================================================
+# Train
+#==============================================================================
+

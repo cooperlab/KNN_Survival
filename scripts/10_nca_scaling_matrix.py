be90652b1e8052c7891c8df11e39bd7c6190a7f3
kheffah <mtageld@emory.edu>
2017-10-01 17:07:55 -0400

    ...

--- scripts/10_nca_scaling_matrix.py
+++ scripts/10_nca_scaling_matrix.py
@@ -20,6 +20,60 @@ import NCA_model as nca
 import KNNSurvival as knn
 
 #%% ===========================================================================
+# Combining results
+#==============================================================================
+
+def combine_results(resultpath_base, n_folds=42):
+    """
+    combine accuracy results from various experiments
+    """
+    
+    print("\nCombining results ...")
+    
+    idxNames = ['method', 'PCA', 'NCA', 'site', 'dtype']
+    folds = list(np.arange(n_folds))
+    folds = ['fold_' + str(j) for j in folds]
+    idxNames.extend(folds)
+    idxNames.extend(['median', 'mean', '25%ile', '75%ile', 'stdev'])
+    
+    CIs = df(index=idxNames)
+    
+    #%%
+    
+    print("\nsite\tdtype\tmethod\tPCA\tNCA")
+    print("-------------------------------")
+    
+    for site in ["GBMLGG", "BRCA", "KIPAN", "MM"]:
+        for dtype in ["Integ", "Gene"]:
+            for method in ["cumulative-time", "non-cumulative"]:
+                for PCA in ["False", "True"]:
+                    for NCA in ["False", "True"]:
+                        
+                        try:
+                            
+                            resultpath = resultpath_base + method + "_" + NCA + "NCA_" + PCA + "PCA/" + \
+                                         site + "_" + dtype + "_/" + site + "_" + dtype + "_testing_Ci.txt"
+                            
+                            ci = np.loadtxt(resultpath, delimiter='\t').reshape([n_folds,])
+                            ci_merge = [method, PCA, NCA, site, dtype]
+                            ci_merge.extend(ci)
+                            ci_merge.extend([np.median(ci), np.mean(ci), \
+                                             np.percentile(ci, 25), np.percentile(ci, 75), \
+                                             np.std(ci)])
+                            
+                            
+                            CIs[CIs.shape[1]] = ci_merge
+                            
+                            print("{}\t{}\t{}\t{}\t{}".format(site, dtype, method, PCA, NCA))
+                        
+                        except FileNotFoundError:
+                            pass
+    # now save
+    CIs.to_csv(resultpath_base + "results_merged.tab", sep="\t")
+
+    print("\nDONE!")
+
+#%% ===========================================================================
 # Primary method - get cross validation accuracy
 #==============================================================================
 
@@ -398,11 +452,11 @@ if __name__ == '__main__':
                         if (site == "MM") and (dtype == "Integ"):
                             continue
                             
-                        if (USE_PCA and (not USE_NCA)):
-                            continue
+                        #if (USE_PCA and (not USE_NCA)):
+                        #    continue
                         
-                        if ((not USE_PCA) and (not USE_NCA)):
-                            continue
+                        #if ((not USE_PCA) and (not USE_NCA)):
+                        #    continue
 
                         if ((dtype == "Gene") and (not USE_PCA)):
                             continue
@@ -435,7 +489,13 @@ if __name__ == '__main__':
                                         USE_BAGGING=USE_BAGGING,
                                         bagging_params=bagging_params)
                                             
+
+                        
+    # Combine results 
+    #=================================================================
     
+    combine_results(RESULTPATH_BASE)
+
 #%%
 #%%
 #%%
--- scripts/9_combine_results.py
+++ scripts/9_combine_results.py
@@ -11,43 +11,55 @@ from pandas import DataFrame as df
 
 #%%
 
-resultpath_base = "/home/mtageld/Desktop/KNN_Survival/Results/5_27Sep2017/"
-
-# rownames
+resultpath_base = "/home/mtageld/Desktop/KNN_Survival/Results/6_28Sep2017/"
 n_folds = 42
-idxNames = ['method', 'PCA', 'NCA', 'site', 'dtype']
-folds = list(np.arange(n_folds))
-folds = ['fold_' + str(j) for j in folds]
-idxNames.extend(folds)
-idxNames.extend(['median', 'mean', '25%ile', '75%ile', 'stdev'])
 
-CIs = df(index=idxNames)
-
-#%%
-for site in ["GBMLGG", "BRCA", "KIPAN", "MM"]:
-    for dtype in ["Integ", "Gene"]:
-        for method in ["cumulative-time", "non-cumulative"]:
-            for PCA in ["False", "True"]:
-                for NCA in ["False", "True"]:
-                    
-                    if ((dtype == "Integ") and (site == "MM")):
-                        continue
-                    if ((dtype == "Gene") and (PCA == "False")):
-                        continue
+def combine_results(resultpath_base, n_folds=42):
+    """
+    combine accuracy results from various experiments
+    """
+    
+    print("\nCombining results ...")
+    
+    idxNames = ['method', 'PCA', 'NCA', 'site', 'dtype']
+    folds = list(np.arange(n_folds))
+    folds = ['fold_' + str(j) for j in folds]
+    idxNames.extend(folds)
+    idxNames.extend(['median', 'mean', '25%ile', '75%ile', 'stdev'])
+    
+    CIs = df(index=idxNames)
+    
+    #%%
+    
+    print("\nsite\tdtype\tmethod\tPCA\tNCA")
+    print("-------------------------------")
+    
+    for site in ["GBMLGG", "BRCA", "KIPAN", "MM"]:
+        for dtype in ["Integ", "Gene"]:
+            for method in ["cumulative-time", "non-cumulative"]:
+                for PCA in ["False", "True"]:
+                    for NCA in ["False", "True"]:
                         
-                    resultpath = resultpath_base + method + "_" + NCA + "NCA_" + PCA + "PCA/" + \
-                                 site + "_" + dtype + "_/" + site + "_" + dtype + "_testing_Ci.txt"
-                    
-                    ci = np.loadtxt(resultpath, delimiter='\t').reshape([n_folds,])
-                    ci_merge = [method, PCA, NCA, site, dtype]
-                    ci_merge.extend(ci)
-                    ci_merge.extend([np.median(ci), np.mean(ci), \
-                                     np.percentile(ci, 25), np.percentile(ci, 75), \
-                                     np.std(ci)])
-                    
-                    
-                    CIs[CIs.shape[1]] = ci_merge
-        
-#%%
+                        try:
+                            
+                            resultpath = resultpath_base + method + "_" + NCA + "NCA_" + PCA + "PCA/" + \
+                                         site + "_" + dtype + "_/" + site + "_" + dtype + "_testing_Ci.txt"
+                            
+                            ci = np.loadtxt(resultpath, delimiter='\t').reshape([n_folds,])
+                            ci_merge = [method, PCA, NCA, site, dtype]
+                            ci_merge.extend(ci)
+                            ci_merge.extend([np.median(ci), np.mean(ci), \
+                                             np.percentile(ci, 25), np.percentile(ci, 75), \
+                                             np.std(ci)])
+                            
+                            
+                            CIs[CIs.shape[1]] = ci_merge
+                            
+                            print("{}\t{}\t{}\t{}\t{}".format(site, dtype, method, PCA, NCA))
+                        
+                        except FileNotFoundError:
+                            pass
+    # now save
+    CIs.to_csv(resultpath_base + "results_merged.tab", sep="\t")
 
-CIs.to_csv(resultpath_base + "results_merged.tab", sep="\t")
+    print("\nDONE!")

1d142429b9d0974eca5d4e8b01aabb106b420e18
Mohamed Amgad <mtageld@emory.edu>
2017-10-08 17:52:04 -0400

    WIP - RSF

---
+++ junk/convert_splitidxs_to_mat.py
@@ -0,0 +1,20 @@
+import numpy as np
+from scipy.io import savemat
+
+basePath = "/home/mohamed/Desktop/CooperLab_Research/KNN_Survival/"
+#basePath = "/home/mtageld/Desktop/KNN_Survival/"
+
+sites = ["GBMLGG", ] #BRCA, KIPAN, MM]
+dtypes = ["Integ", ] #Gene]
+
+for site in sites:
+    for dtype in dtypes:
+
+        print(site + "\t" + dtype)
+        
+        dataPath = basePath + 'Data/SingleCancerDatasets/' + site + '/' + site + '_' + dtype + '_Preprocessed.mat'
+        splitIdxPath = dataPath.split('.mat')[0] + '_splitIdxs.pkl'
+        
+        splitIdxs = np.load(splitIdxPath)
+        
+        savemat(splitIdxPath.split('.pkl')[0] + '.mat', splitIdxs)

d6a3ea81da5df98d7f7cc8ab4f29ccef1e517dae
Mohamed Amgad <mtageld@emory.edu>
2017-10-09 14:57:32 -0400

    fixed bug in saving RSF preds per fold

--- comparative_methods/RSFs.R
+++ comparative_methods/RSFs.R
@@ -7,8 +7,8 @@ library(survival, quietly=TRUE)
 library(R.matlab, quietly=TRUE)
 
 # paths
-#basePath = "/home/mohamed/Desktop/CooperLab_Research/KNN_Survival/"
-basePath = "/home/mtageld/Desktop/KNN_Survival/"
+basePath = "/home/mohamed/Desktop/CooperLab_Research/KNN_Survival/"
+#basePath = "/home/mtageld/Desktop/KNN_Survival/"
 
 # data description
 sites = c("GBMLGG", "BRCA", "KIPAN")
@@ -27,6 +27,10 @@ for (dtype in dtypes) {
     cat(site, dtype, "\n")
     cat("============================================\n")
     
+    # Create output dirs
+    result_dir = paste(basePath, 'Results/9_8Oct2017/', site, '_', dtype, '/', sep="")
+    dir.create(result_dir, showWarnings = TRUE, recursive = FALSE, mode = "0777")
+    
     # Reading the matfiles
     dpath = paste(basePath, 'Data/SingleCancerDatasets/', site, '/', 
                   site, '_', dtype, '_Preprocessed', sep="")
@@ -43,13 +47,6 @@ for (dtype in dtypes) {
                            status = 1-Data$Censored[1,])
     }
     
-    # initialize accuracy
-    # Note that we can't initialize to a fized length
-    # bacuse some folds are larger than others, so we'll
-    # just init to max possible no of patients
-    preds_val = matrix(nrow=length(Data$Survival), ncol=30)
-    preds_test = matrix(nrow=length(Data$Survival), ncol=30)
-    
     # Go through folds
     for (fold in 1:30){
     
@@ -65,10 +62,17 @@ for (dtype in dtypes) {
         testIdxs = splitIdxs$test[[fold]][[1]][1,]  + 1
       }
       
+      # initialize predictions
+      n_hyperparam_sets = length(treeArray) * length(nodeSizeArray) * length(nSplitArray)
+      preds_val = matrix(nrow=length(validIdxs), ncol=n_hyperparam_sets)
+      preds_test = matrix(nrow=length(testIdxs), ncol=n_hyperparam_sets)
+      
       # Go through hyperparams and get Ci
       
       cat("\nfold | trees | nodes | Split |\n")
       cat("--------------------------------------------\n")
+      
+      hyperp_idx = 1
       
       for (i in 1:length(treeArray)){
         for (j in 1:length(nodeSizeArray)){
@@ -83,36 +87,34 @@ for (dtype in dtypes) {
                                ntree = treeArray[i])
             
             # predict validation set
-            preds_val[1:length(validIdxs),fold] = 
+            preds_val[1:length(validIdxs),hyperp_idx] = 
               predict.rfsrc(train.obj, allInfo[validIdxs,])$predicted
             
             # test
-            preds_test[1:length(testIdxs),fold] = 
+            preds_test[1:length(testIdxs),hyperp_idx] = 
               predict.rfsrc(train.obj, allInfo[testIdxs,])$predicted
             
-            # clear space      
+            # clear space and increment      
             rm(train.obj)
+            hyperp_idx = hyperp_idx + 1
       
           }
         }
       }
+      
+      # save result
+      preds_val = data.frame(preds_val)
+      preds_test = data.frame(preds_test)
+      resultFile_val = paste(result_dir, 'fold_', fold, '_preds_val.txt', sep="")
+      resultFile_test = paste(result_dir, 'fold_', fold, '_preds_test.txt', sep="")
+      write.table(preds_val, file=resultFile_val)
+      write.table(preds_test, file=resultFile_test)
+      
+      # clear space
+      rm(preds_val)
+      rm(preds_test)
+      
     }
-    
-    # save result
-    resultFile_val = paste(basePath, 'Results/9_8Oct2017/', 
-                       site, '_', dtype, '_preds_val.txt', sep="")
-    resultFile_test = paste(basePath, 'Results/9_8Oct2017/', 
-                           site, '_', dtype, '_preds_test.txt', sep="")
-    
-    preds_val = data.frame(preds_val)
-    preds_test = data.frame(preds_test)
-    
-    write.table(preds_val, file=resultFile_val)
-    write.table(preds_test, file=resultFile_test)
-    
-    cat("\nPreds is stored to: \n", 
-        resultFile_val, "\n", resultFile_test, "\n")
-
   }
 }
 

be339b7aa1c6e9eafe305e06e3bfff8488c9c09c
kheffah <mtageld@emory.edu>
2017-10-09 14:59:45 -0400

    ..

--- comparative_methods/RSFs.R
+++ comparative_methods/RSFs.R
@@ -7,8 +7,8 @@ library(survival, quietly=TRUE)
 library(R.matlab, quietly=TRUE)
 
 # paths
-basePath = "/home/mohamed/Desktop/CooperLab_Research/KNN_Survival/"
-#basePath = "/home/mtageld/Desktop/KNN_Survival/"
+#basePath = "/home/mohamed/Desktop/CooperLab_Research/KNN_Survival/"
+basePath = "/home/mtageld/Desktop/KNN_Survival/"
 
 # data description
 sites = c("GBMLGG", "BRCA", "KIPAN")

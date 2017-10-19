eb560a6338e7fd87218499a585457fda4c5f8259
Mohamed Amgad <mtageld@emory.edu>
2017-10-19 01:02:26 -0400

    ...

--- NCA_model.py
+++ NCA_model.py
@@ -484,7 +484,7 @@ class SurvivalNCA(object):
                         # Get at-risk mask (to be multiplied by Pij)
                         n_batch = t_batch.shape[0]
                         Pij_mask = np.zeros((n_batch, n_batch))
-                        for idx in range(len(t_batch)):
+                        for idx in range(n_batch):
                             # only observed cases
                             if o_batch[idx] == 1:
                                 # only at-risk cases

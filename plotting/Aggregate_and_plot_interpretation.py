68b56c9a8769cfdcb977a959c95b97905556473b
Mohamed Amgad <mtageld@emory.edu>
2017-10-21 02:29:23 -0400

    DONE - plotting ranks

--- plotting/Aggregate_and_plot_interpretation.py
+++ plotting/Aggregate_and_plot_interpretation.py
@@ -5,7 +5,7 @@ Created on Thu Oct 19 17:36:05 2017
 @author: mohamed
 """
 
-import sys
+#import sys
 import os
 import numpy as np
 from pandas import read_table
@@ -69,21 +69,21 @@ feature_ranks = np.zeros((len(fnames), len(ranks)))
 
 for fidx, fname in enumerate(fnames):
     
-    print("feature {} of {}: {}".format(fidx, len(fnames)-1, fname))
+    # print("feature {} of {}: {}".format(fidx, len(fnames)-1, fname))
     
     this_feature_ranks = []
     
     for fold, fold_ranks in enumerate(ranks):
-        
-        feature_ranks[fidx, fold] = fold_ranks.index(fname)
+        feature_ranks[fidx, fold] = fold_ranks.index(fname) + 1
         
     # sort so that lower-ranked folds appear first (for this feature)
     feature_ranks[fidx, :].sort()
 
+# no of "informative"/less-noisy folds to use
 n_top_folds = 10
 
+# Get ranks and sort
 overall_ranks = np.mean(feature_ranks[:, 0:n_top_folds], axis=1)
-
 sorted_idxs = np.argsort(overall_ranks)
 overall_ranks = overall_ranks[sorted_idxs]
 feature_ranks = feature_ranks[sorted_idxs, :]
@@ -94,31 +94,52 @@ fnames_sorted = np.array(fnames)[sorted_idxs]
 # Plotting
 #==============================================================================
 
+# number of top features to plot
 n_top = 30
-#bp = plt.boxplot(feature_ranks[0:n_top, :].T, patch_artist=True, showfliers=False)
 
-plt.plot(overall_ranks[0:n_top], linewidth=1.5, color='r', linestyle='-')
+plt.plot(overall_ranks[0:n_top], linewidth=2, color='r', linestyle='-')
 
+# scatter fold ranks
 for fidx in range(n_top):
     plt.scatter(x=fidx * np.ones((n_top_folds,)), 
                 y=feature_ranks[fidx, 0:n_top_folds], 
                 alpha=0.4, color='grey')
-                
+
+# Add top feature names                
 for i, txt in enumerate(fnames_sorted[0:n_top]):
     
     if len(txt) > 20:
         txt = txt.split('-')[-1]
     txt = txt.replace('_', ' ')
     
+    # color code different data types
     if 'Mut' in txt:
         col = 'r'
+    elif 'CNVArm' in txt:
+        col = 'purple'
     elif 'CNV' in txt:
         col = 'b'
     else:
         col = 'k'
     
-    plt.annotate(txt.replace('_', ' '), (i - 0.5, 15 + overall_ranks[i]), 
+    # now add text annotations
+    plt.annotate(txt.replace('_', ' '), (i - 0.5, 10 + overall_ranks[i]), 
                  va= 'bottom',
                  rotation= 'vertical',
-                 fontsize= 8,
+                 fontsize= 9,
                  color=col)
+                 
+# fix ylabel and range
+plt.ylabel("Rank", fontsize=14)
+plt.ylim(ymin=0, ymax=50)
+
+# remove x ticks (color coded instead)
+plt.xlim(xmin=-1, xmax=n_top)
+plt.xticks(np.arange(n_top), [])
+
+# plot title
+plt.title(site + ' ' + dtype, fontsize=16, fontweight='bold')
+
+# Save
+plt.savefig(save_path + '_featranks.svg', figsize=(6, 6), bbox_inches='tight')
+plt.close()

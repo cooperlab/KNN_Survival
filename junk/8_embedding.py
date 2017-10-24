e3b98bd1cc5d6e09b7f064c60e82913f37295cb7
Mohamed Amgad <mtageld@emory.edu>
2017-10-24 15:13:32 -0400

    WIP - embedding

--- junk/8_embedding.py
+++ junk/8_embedding.py
@@ -94,14 +94,41 @@ def isolate_feats(fnames, thresholds):
 #------------------------------------------------------------------------------
 
 # IDHwt
-#is_IDHwt = isolate_feats(fnames=("IDH1_Mut", "IDH2_Mut"), thresholds=(0, 0))
-is_IDHwt = isolate_feats(fnames=("IDH1_Mut", ), thresholds=(0, ))
+is_IDHwt = isolate_feats(fnames=("IDH1_Mut", "IDH2_Mut"), thresholds=(0, 0))
 is_IDHwt = 1 - np.int32(is_IDHwt > 0)
 
 # CIC
 is_CIC = isolate_feats(fnames=("CIC_Mut",), thresholds=(0,))
 is_CIC = np.int32(is_CIC > 0)
 
+# GBM vs LGG
+#------------------------------------------------------------------------------
+
+# LGG
+GBMfeats = ('histological_type-Is-glioblastoma',
+            'histological_type-Is-treated',
+            'histological_type-Is-untreated',)
+is_LGG = isolate_feats(fnames=GBMfeats, thresholds=(0, 0, 0))
+is_LGG = 1 - np.int32(is_LGG > 0)
+
+
+# LGG Subtypes
+#------------------------------------------------------------------------------
+
+# 1p19q
+is_1p19qCodel = isolate_feats(fnames=("1p_CNVArm", "19q_CNVArm"), thresholds=(0, 0))
+is_1p19qCodel = 1 - np.int32(is_1p19qCodel == 2)
+
+# codels and non-codels
+IDHmut = 1 - is_IDHwt
+NonCodel = 1 - is_1p19qCodel
+IDHmutNonCodel = np.int32((IDHmut + NonCodel) == 2)
+IDHmutCodel = np.int32((IDHmut + is_1p19qCodel) == 2)
+
+# only keep LGG
+LGG_IDHwt = np.int32((is_LGG + is_IDHwt) == 2)
+LGG_IDHmutNonCodel = np.int32((is_LGG + IDHmutNonCodel) == 2)
+LGG_IDHmutCodel = np.int32((is_LGG + IDHmutCodel) == 2)
 
 #%%
 # Fetch embedding and plot
@@ -119,25 +146,40 @@ for embed_idx, embed_fname in enumerate(embedding_files):
     ci_valid = np.loadtxt(accuracy_path + accuracy_files[embed_idx])
     
     # plot and save
+    
+    # GBM vs. LGG
+    plt.scatter(embedding[is_LGG==1, 0], embedding[is_LGG==1, 1], c='b')
+    plt.scatter(embedding[is_LGG==0, 0], embedding[is_LGG==0, 1], c='r')
+    plt.title("GBMLGG - ci_valid = {}, n_epochs = {}".format(round(ci_valid[-1], 3), len(ci_valid)), fontsize=16)
+    plt.savefig(result_path + '/tmp/' + embed_fname.split('.npy')[0] + '_GBMLGG.svg')
+    plt.close()
 
     # IDH only
     plt.scatter(embedding[is_IDHwt==0, 0], embedding[is_IDHwt==0, 1], c='b')
     plt.scatter(embedding[is_IDHwt==1, 0], embedding[is_IDHwt==1, 1], c='r')
     plt.title("IDH - ci_valid = {}, n_epochs = {}".format(round(ci_valid[-1], 3), len(ci_valid)), fontsize=16)
-    plt.savefig(result_path + '/tmp/IDH/' + embed_fname.split('.npy')[0] + '.svg')
+    plt.savefig(result_path + '/tmp/' + embed_fname.split('.npy')[0] + '_IDH.svg')
     plt.close()
     
     # CIC only
     plt.scatter(embedding[is_CIC==0, 0], embedding[is_CIC==0, 1], c='k')
     plt.scatter(embedding[is_CIC==1, 0], embedding[is_CIC==1, 1], c='gold')
     plt.title("CIC - ci_valid = {}, n_epochs = {}".format(round(ci_valid[-1], 3), len(ci_valid)), fontsize=16)
-    plt.savefig(result_path + '/tmp/CIC/' + embed_fname.split('.npy')[0] + '.svg')
+    plt.savefig(result_path + '/tmp/' + embed_fname.split('.npy')[0] + '_CIC.svg')
     plt.close()
     
-    # IDH1 and CIC
+    # IDH and CIC
     plt.scatter(embedding[is_IDHwt==0, 0], embedding[is_IDHwt==0, 1], c='k')
     plt.scatter(embedding[is_CIC==1, 0], embedding[is_CIC==1, 1], c='gold')
     plt.scatter(embedding[is_IDHwt==1, 0], embedding[is_IDHwt==1, 1], c='r')
     plt.title("IDH-CIC - ci_valid = {}, n_epochs = {}".format(round(ci_valid[-1], 3), len(ci_valid)), fontsize=16)
-    plt.savefig(result_path + '/tmp/IDH-CIC/' + embed_fname.split('.npy')[0] + '.svg')
+    plt.savefig(result_path + '/tmp/' + embed_fname.split('.npy')[0] + '_IDH-CIC.svg')
+    plt.close()
+    
+    # LGGsubtypes
+    plt.scatter(embedding[LGG_IDHmutNonCodel==1, 0], embedding[LGG_IDHmutNonCodel==1, 1], c='k')
+    plt.scatter(embedding[LGG_IDHmutCodel==1, 0], embedding[LGG_IDHmutCodel==1, 1], c='gold')
+    plt.scatter(embedding[LGG_IDHwt==1, 0], embedding[LGG_IDHwt==1, 1], c='r')
+    plt.title("LGGsubtypes - ci_valid = {}, n_epochs = {}".format(round(ci_valid[-1], 3), len(ci_valid)), fontsize=16)
+    plt.savefig(result_path + '/tmp/' + embed_fname.split('.npy')[0] + '_LGGsubtypes.svg')
     plt.close()
